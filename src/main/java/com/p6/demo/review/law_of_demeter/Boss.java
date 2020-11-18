6bbbecaf90fa6bca7f57b4b7a26fec7c98fcc41f
xusisi <757507768@qq.com>
2020-11-18 15:28:32 +0800

    迪米特法则
    2020年11月18日15:28:31

---
+++ src/main/java/com/p6/demo/review/law_of_demeter/Boss.java
@@ -0,0 +1,40 @@
+package com.p6.demo.review.law_of_demeter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 徐恩晗 xshlxx@126.com
+ * @since 2020/11/18
+ */
+public class Boss {
+
+    public void commandCheckNumber(TeamLeader teamLeader) {
+
+        // 模拟BOSS一页一页往下翻页，TeamLeader实时统计
+        List<Course> courseList = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+
+            courseList.add(new Course());
+        }
+
+        teamLeader.checkNumberOfCourses(courseList);
+    }
+
+    // 重载过后的方法
+    public void commandCheckNumber2(TeamLeader teamLeader) {
+
+        teamLeader.checkNumberOfCourses();
+    }
+
+
+    public static void main(String[] args) {
+
+        Boss boss = new Boss();
+
+        TeamLeader teamLeader = new TeamLeader();
+
+        boss.commandCheckNumber(teamLeader);
+    }
+}
--- src/main/java/com/p6/demo/review/law_of_demeter/TeamLeader.java
+++ src/main/java/com/p6/demo/review/law_of_demeter/TeamLeader.java
@@ -1,5 +1,6 @@
 package com.p6.demo.review.law_of_demeter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,5 +12,17 @@ public class TeamLeader {
     public void checkNumberOfCourses(List<Course> courseList) {
 
         System.out.println("目前已发布的课程数量是:" + courseList.size());
+    }
+
+    public void checkNumberOfCourses() {
+
+        List<Course> courseList = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+
+            courseList.add(new Course());
+        }
+
+        System.out.println("目前已发布的课程数量是:" + courseList);
     }
 }
