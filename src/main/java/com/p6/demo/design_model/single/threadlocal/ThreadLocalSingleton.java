c82d5b5843816471d790679befc2cab05d7fb996
xusisi <757507768@qq.com>
2020-11-26 09:29:15 +0800

    Revert "Revert "竞拍大厅分页""
    This reverts commit 95f0b535277d8524551386f5f16a54a9fdbaefea.

---
+++ src/main/java/com/p6/demo/design_model/single/threadlocal/ThreadLocalSingleton.java
@@ -0,0 +1,25 @@
+package com.p6.demo.design_model.single.threadlocal;
+
+/**
+ * @author 徐恩晗 xshlxx@126.com
+ * @since 2020/11/25
+ */
+public class ThreadLocalSingleton {
+
+
+    private static final ThreadLocal<ThreadLocalSingleton> threadLocalInstance = new ThreadLocal<ThreadLocalSingleton> (){
+
+        @Override
+        protected ThreadLocalSingleton initialValue() {
+
+            return new ThreadLocalSingleton();
+        }
+    };
+
+    private ThreadLocalSingleton() {}
+
+    public static ThreadLocalSingleton getInstance() {
+
+        return threadLocalInstance.get();
+    }
+}
